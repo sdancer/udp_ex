57214d630dd1113ab020c439cac5da9f47edb0ba
SD <ktion23@gmail.com>
2019-12-30 22:18:21 +0800

    .

--- lib/serv/servtcp.ex
+++ lib/serv/servtcp.ex
@@ -12,11 +12,11 @@ defmodule ServTcp do
 
         {:ok, data} = :gen_tcp.recv clientSocket, 0
 
-        {rc4stream_s, decoded} = :crypto.stream_encrypt rc4stream_d, data
+        {rc4stream_d, decoded} = :crypto.stream_encrypt rc4stream_d, data
         <<
             0, sessionid ::64-little, 0::64, 0::64, 0::64, 0::64,
         >> = decoded
-        IO.inspect {__MODULE__, :got_session, sessionid}
+        IO.inspect {__MODULE__, :got_session, sessionid, decoded}
         :gen_tcp.send clientSocket, "hello world"
 
 
@@ -27,13 +27,14 @@ defmodule ServTcp do
     end
 
     def handle_info {:tcp, socket, bin}, state do
-        {rc4stream_s, decoded} = :crypto.stream_encrypt state.rc4stream_d, bin
+        {rc4stream_d, decoded} = :crypto.stream_encrypt state.rc4stream_d, bin
 
         IO.inspect {__MODULE__, "got data", socket, decoded}
 
         #send state.session, {:tcp_data, self(), decoded}
 
-        #send to session
+        state = Map.merge state, %{rc4stream_d: rc4stream_d}
+
         {:noreply, state}
     end
 
