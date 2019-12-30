c47430818400c28e42c6e528e8f3b1e1e1acb92d
SD <ktion23@gmail.com>
2019-12-31 00:09:09 +0800

    .

---
+++ lib/serv/serv_udp.ex
@@ -0,0 +1,24 @@
+defmodule ServerUdp do
+
+    def start(port, client_session) do
+        server(port, client_session)
+    end
+
+    def server(port, client_session) do
+       {:ok, socket} = :gen_udp.open(port, [:binary, {:active, false}])
+       spawn UdpServer, :loop, [socket, client_session]
+
+       {:ok, socket}
+    end
+
+    def loop(socket, client_session) do
+       :inet.setopts(socket, [{:active, :once}])
+       receive do
+          {:udp, socket, host, port, bin} ->
+              IO.inspect {__MODULE__, "received", host, port, bin}
+              send client_session, {:udp_data, host, port, bin}
+       end
+
+       __MODULE__.loop(socket, client_session)
+    end
+end
